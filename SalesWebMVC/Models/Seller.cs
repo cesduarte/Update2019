f6be7a2008b72679ffb49e65aadaa771acb9413c
cesduarte <59450791+cesduarte@users.noreply.github.com>
2020-03-21 22:33:30 -0300

    Foreign key not null (referential integrity)

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -14,6 +14,7 @@ namespace SalesWebMVC.Models
         public double BaseSalary { get; set; }
 
         public Department Department { get; set; }
+        public int Departmentid { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
