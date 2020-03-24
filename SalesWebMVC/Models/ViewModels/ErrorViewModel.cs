e4f5b507af0b6cff6c7604c5e94cd0677dec8901
cesduarte <59450791+cesduarte@users.noreply.github.com>
2020-03-23 23:40:50 -0300

    Returning custom error page

--- SalesWebMVC/Models/ViewModels/ErrorViewModel.cs
+++ SalesWebMVC/Models/ViewModels/ErrorViewModel.cs
@@ -6,6 +6,11 @@ namespace SalesWebMVC.Models.ViewModels
     {
         public string RequestId { get; set; }
 
+        public string  Message { get; set; }
+
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+
+
     }
 }
